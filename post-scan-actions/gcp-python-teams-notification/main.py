c144cecbe20586e983065fac4ee8cc323717c371
George Davis <george_davis@trendmicro.com>
2022-07-27 20:11:53 -0400

    Finalizing plugin code

--- post-scan-actions/gcp-python-teams-notification/main.py
+++ post-scan-actions/gcp-python-teams-notification/main.py
@@ -1,68 +1,117 @@
-import urllib3
-import json
 import os
+import json
+import base64
+import urllib3
+from http.client import responses
+
 http = urllib3.PoolManager()
 
-def lambda_handler(event, context):
+def get_gcp_project_id(resource_name):
+    return str(resource_name).split("/")[1]
 
-    url = os.environ['TEAMS_URL']
+def get_bucket_name_from_file_url(file_url):
+    return str(file_url.split("/")[-2:][0])
 
-    for record in event['Records']:
+def get_file_name_from_file_url(file_url):
+    return str(file_url.split("/")[-1:][0])
 
-        #Message details from SNS event
-        message = json.loads(record['Sns']['Message'])
-        findings = message['scanning_result'].get('Findings')
+def build_file_metadata_url(project_id, bucket_name, file_name):
+    return "https://console.cloud.google.com/storage/browser/_details/" + bucket_name + "/" + file_name + "?project=" + project_id
 
-        #ARN info to get AWS Account ID
-        arn = json.dumps(record['EventSubscriptionArn'])
-        account_id = arn.split(":")[4].strip()
+def main(event, context):
 
-        if findings:
+    try:
 
-            malwares = []
-            types = []
-            for finding in message['scanning_result']['Findings']:
-                malwares.append(finding.get('malware'))
-                types.append(finding.get('type'))
+        print(f'Context: {context}')
+        print(f'Event: {event}')
+        base64_data = event.get('data', '')
+        base64_bytes = base64_data.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        message = json.loads(message_bytes.decode('ascii'))
+        print(f'Message: {message}')
 
-            account=str(account_id)
+        url = os.environ['TEAMS_URL']
 
-            malwares=', '.join(malwares)
+        print("""This Function was triggered by messageId {} published at {} to {}""".format(context.event_id, context.timestamp, context.resource))
 
-            types=', '.join(types)
+        if message:
+            # Message details from the Pub/Sub topic publish event
+            findings = message['scanning_result'].get('Findings')
 
-            file_url=str(message['file_url'])
+            if findings:
 
+                project_id = get_gcp_project_id(resource_name=context.resource)
+                bucket_name = get_bucket_name_from_file_url(file_url=str(message['file_url']))
+                file_name = get_file_name_from_file_url(file_url=str(message['file_url']))
+                file_metadata_url=str(build_file_metadata_url(project_id=project_id, bucket_name=bucket_name, file_name=file_name))
 
-            payload = {
-                "summary":"Malicious Object Detected",
-                "sections":[
-                    {
-                        "activityTitle":"A <b>Malicious object</b> has been detected!"
-                    },
-                    {
-                        "markdown": False,
-                        "facts":[
+                malwares = []
+                types = []
+                for finding in message['scanning_result']['Findings']:
+                    malwares.append(finding.get('malware'))
+                    types.append(finding.get('type'))
+
+                malwares=', '.join(malwares)
+                types=', '.join(types)
+
+                payload = {
+                    "@type": "MessageCard",
+                    "@context": "http://schema.org/extensions",
+                    'summary': 'Malicious Object Detected',
+                    'sections': [
                         {
-                            "name":"Account ID:",
-                            "value":account
+                            'activityTitle': 'A <b>Malicious object</b> has been detected!'
                         },
                         {
-                            "name":"Malware Name(s):",
-                            "value":malwares
-                        },
-                        {
-                            "name":"Malware Type(s):",
-                            "value":types
-                        },
-                        {
-                            "name":"Location:",
-                            "value":file_url
+                            'markdown': False,
+                            'facts': [
+                                {
+                                    'name': 'GCP Project ID: ',
+                                    'value': project_id
+                                },
+                                {
+                                    'name': 'GCP Bucket Name: ',
+                                    'value': bucket_name
+                                },
+                                {
+                                    'name': 'File Name: ',
+                                    'value': file_name
+                                },
+                                {
+                                    'name': 'Malware Name(s): ',
+                                    'value': malwares
+                                },
+                                {
+                                    'name': 'Malware Type(s): ',
+                                    'value': types
+                                },
+                                {
+                                    'name': 'File URL: ',
+                                    'value': file_metadata_url
+                                }
+                            ]
                         }
-                        ]
-                    }
-                ]
-            }
+                    ],
+                    'potentialAction': [
+                        {
+                            '@type': 'OpenUri',
+                            'name': 'View File Metadata',
+                            'targets': [
+                                {
+                                    'os': 'default',
+                                    'uri': file_metadata_url
+                                }
+                            ]
+                        }
+                    ]
+                }
 
-            encoded_msg = json.dumps(payload).encode('utf-8')
-            resp = http.request('POST',url, body=encoded_msg)
+                encoded_message = json.dumps(payload).encode('utf-8')
+                resp = http.request('POST', url,  body=encoded_message)
+
+                if resp.status != 200:
+                    raise Exception("HTTP Error " + str(resp.status) + ". Message: " + str(responses[resp.status]))
+                return resp.status
+
+    except Exception as e:
+        print("Error: ", str(e))
