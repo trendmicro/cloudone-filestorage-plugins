60c78fc7bbf5bf9092c758a9ba88623be68f402a
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2022-10-10 12:10:13 -0500

    feat(post-scan action): Remove conflicts from previous PR

--- post-scan-actions/aws-python-promote-or-quarantine/terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-# Outputs
-output "ScanResult" {
-   value = aws_lambda_function.PromoteOrQuarantineLambda.arn
-}
-
-output "PromoteOrQuarantineLambda" {
-   value = aws_lambda_function.PromoteOrQuarantineLambda.arn
-}
