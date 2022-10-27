475d85d8ec4cd215883a2b761dfbc308831a81c7
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2022-10-27 15:45:39 -0500

    Move the policy to a data source

--- post-scan-actions/aws-python-slack-notification/terraform/iam.tf
+++ post-scan-actions/aws-python-slack-notification/terraform/iam.tf
@@ -1,20 +1,19 @@
+# Lambda execution policy for the IAM role
+data "aws_iam_policy_document" "SlackNotificationLambdaPolicy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+
+    }
+  }
+}
+
 # Lambda execution role
 resource "aws_iam_role" "SlackNotificationLambdaRole" {
   name_prefix   = "SlackNotificationLambdaRole-"
   description = "AWS IAM Role for managing aws lambda"
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "lambda.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
+  assume_role_policy = data.aws_iam_policy_document.SlackNotificationLambdaPolicy.json
 }
