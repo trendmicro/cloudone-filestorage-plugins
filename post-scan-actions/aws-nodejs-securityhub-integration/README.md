86bcb4c28eeeca8ee7fa27462e47c3c3242b141c
Andrew_C_Lee <105200856+andrew-c-lee@users.noreply.github.com>
2023-10-25 16:50:55 +0800

    feat: Update AWS security hub plugin to node18
    * Upgrade lambda runtime from node12 to node18
    * Made changes to build process so the lambda code package can be slimmer and only contain necessary parts
    * Upgrade from aws sdk v2 to aws sdk v3 since lambda node18 runtime only contains v3

--- post-scan-actions/aws-nodejs-securityhub-integration/README.md
+++ post-scan-actions/aws-nodejs-securityhub-integration/README.md
@@ -2,11 +2,11 @@
 
 Pushes FSS malware detections to AWS Security Hub integration.
 
-Change directory to `/lambda`. Run `npm run package` to install dependencies and zip it all together as `bundle.zip` and upload it to a bucket and take note of the key.
+Run `npm run package` to install dependencies and zip it all together as `bundle.zip` and upload it to a bucket and take note of the key.
 Start a new stack based on this template.
 
 ## Template Inputs
 
- * `snsScanResultTopicArn:`   FSS Results SNS Topic ARN
- * `LambdaZipBucket`          S3 bucket for lambda zip. If your have it under s3://bucket/path/to/zip.zip, enter 'bucket' here.
- * `LambdaZipKey`             S3 key for lambda zip. If your have it under s3://bucket/path/to/zip.zip, enter 'path/to/zip.zip' here. 
+* `snsScanResultTopicArn:`   FSS Results SNS Topic ARN
+* `LambdaZipBucket`          S3 bucket for lambda zip. If your have it under s3://bucket/path/to/zip.zip, enter 'bucket' here.
+* `LambdaZipKey`             S3 key for lambda zip. If your have it under s3://bucket/path/to/zip.zip, enter 'path/to/zip.zip' here.
---
+++ post-scan-actions/aws-nodejs-securityhub-integration/esbuild.config.js
@@ -0,0 +1,69 @@
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+const rimraf = require('rimraf')
+const fs = require('fs')
+const path = require('path')
+const archiver = require('archiver')
+
+const OUT_DIR = 'dist'
+
+async function main() {
+  rimraf.sync(OUT_DIR)
+  rimraf.sync('bundle.zip')
+
+  await require('esbuild')
+    .build({
+      entryPoints: ['lambda/index.js'],
+      outfile: 'dist/index.js',
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      format: 'cjs',
+      platform: 'node',
+      keepNames: true,
+      plugins: [
+        nodeExternalsPlugin({
+          // This will include dependencies in the bundle
+          dependencies: false
+        })
+      ],
+      // Exclude aws sdk v3 as node18.x runtime has aws sdk v3 already
+      external: ['@aws-sdk/*'],
+      treeShaking: true,
+      metafile: true
+    })
+    .catch((err) => {
+      console.error(err)
+      process.exit(1)
+    })
+
+  await zipDirectory(`${OUT_DIR}/`, 'bundle.zip')
+}
+
+function zipDirectory(source, outputTarget) {
+  const archive = archiver('zip', { zlib: { level: 9 }, store: true });
+  const stream = fs.createWriteStream(outputTarget, { flags: 'w' })
+
+  stream.on("close", () => {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.')
+  });
+
+  return new Promise(async (resolve, reject) => {
+    archive.pipe(stream)
+    archive.on("error", err => reject(err))
+    archive.directory(source, false)
+    await archive.finalize()
+    resolve()
+  })
+}
+
+console.time('Built in')
+
+main()
+  .then(() => {
+    console.timeEnd('Built in')
+  })
+  .catch((err) => {
+    console.error(err)
+    throw err
+  })
