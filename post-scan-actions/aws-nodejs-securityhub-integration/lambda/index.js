28fd0ceff3bb90411950e78cbd520d4027e34030
Andrew C Lee <105200856+andrew-c-lee@users.noreply.github.com>
2023-10-26 17:21:20 +0800

    feat: Update AWS security hub plugin to node18 (#157)
    * Upgrade lambda runtime from node12 to node18
    * Made changes to build process so the lambda code package can be slimmer and only contain necessary parts
    * Upgrade from aws sdk v2 to aws sdk v3 since lambda node18 runtime only contains v3

--- post-scan-actions/aws-nodejs-securityhub-integration/lambda/index.js
+++ post-scan-actions/aws-nodejs-securityhub-integration/lambda/index.js
@@ -1,25 +1,25 @@
-const AWS = require('aws-sdk');
-const uuid = require('uuid');
+const { v4: uuidV4 } = require('uuid')
+const { SecurityHubClient, BatchImportFindingsCommand } = require('@aws-sdk/client-securityhub')
 
 const parseDetectionPath = (path) => {
-  const bucket = path.substring(8, path.indexOf('.s3.'));
-  const key = path.split('/').slice(3).join('/');
+  const bucket = path.substring(8, path.indexOf('.s3.'))
+  const key = path.split('/').slice(3).join('/')
   return {
     bucket,
     key
-  };
-};
+  }
+}
 
-const publishToSecurityHub = async (securityhub, securityHubArn, accountId, malwareName, bucket, key ) => {
-  const date = new Date().toISOString();
+const publishToSecurityHub = async (securityhub, securityHubArn, accountId, malwareName, bucket, key) => {
+  const date = new Date().toISOString()
   const params = {
-    Findings : [
+    Findings: [
       {
         AwsAccountId: accountId,
         CreatedAt: date,
         Description: "Malware found!",
         GeneratorId: "FSS",
-        Id: `${accountId}/${uuid.v4()}`,
+        Id: `${accountId}/${uuidV4()}`,
         ProductArn: securityHubArn,
         SchemaVersion: "2018-10-08",
         Resources: [
@@ -32,14 +32,14 @@ const publishToSecurityHub = async (securityhub, securityHubArn, accountId, malw
             Type: "AwsS3Bucket"
           },
         ],
-        Malware: [ 
-          { 
-              Name: malwareName,
-              Path: key,
-              State: "OBSERVED",
-              Type: "POTENTIALLY_UNWANTED"
+        Malware: [
+          {
+            Name: malwareName,
+            Path: key,
+            State: "OBSERVED",
+            Type: "POTENTIALLY_UNWANTED"
           }
-      ],
+        ],
         Severity: {
           Label: "HIGH"
         },
@@ -50,41 +50,42 @@ const publishToSecurityHub = async (securityhub, securityHubArn, accountId, malw
         UpdatedAt: date
       }
     ]
-  };
+  }
+
   try {
-    const result = await securityhub.batchImportFindings(params).promise();
-    console.log(result);
-    return result;
+    const command = new BatchImportFindingsCommand(params)
+    const result = await securityhub.send(command)
+    console.log('result: ', result)
+    return result
   } catch (error) {
-    console.error(error);
-    return error;
+    console.error(error)
+    return error
   }
 };
 
 exports.handler = async (event) => {
-  const securityHubArn = process.env.SECURITY_HUB_ARN;
-  const accountId = process.env.ACCOUNT_ID;
-  const securityhub = new AWS.SecurityHub();
+  const securityHubArn = process.env.SECURITY_HUB_ARN
+  const accountId = process.env.ACCOUNT_ID
+  const securityhub = new SecurityHubClient()
   return Promise.all(event.Records.map(async (record) => {
-    console.log(record);
-    const message = JSON.parse(record.Sns.Message);
-    const findings = message.scanning_result.Findings;
-    
-    const results = [];
-    
+    const message = JSON.parse(record.Sns.Message)
+    const findings = message.scanning_result.Findings
+
+    const results = []
+
     if (findings) {
       findings.forEach(finding => {
-        const malwareName = finding.malware;
-        const path = parseDetectionPath(message.file_url);
-        console.log(path);
-        const findingResult = publishToSecurityHub(securityhub, securityHubArn, accountId, malwareName, path.bucket, path.key);
-        results.push(findingResult);
-        console.log('findingResult: ', findingResult);
-      });
+        const malwareName = finding.malware
+        const path = parseDetectionPath(message.file_url)
+        console.log('Detected path: ', path)
+        const findingResult = publishToSecurityHub(securityhub, securityHubArn, accountId, malwareName, path.bucket, path.key)
+        results.push(findingResult)
+        console.log('findingResult: ', findingResult)
+      })
     }
-    console.log(results);
-    if (results){
-      await Promise.all(results);
+
+    if (results) {
+      await Promise.all(results)
     }
-  }));
+  }))
 };
